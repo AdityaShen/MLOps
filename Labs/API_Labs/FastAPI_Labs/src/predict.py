66b14f3ebf2626ef2631cea78597e2fb7502b534
Aditya Shenoy <79261869+AdityaShen@users.noreply.github.com>
2026-03-13 23:19:25 -0400

    Refactor predict function to use scaler and model
    Signed-off-by: Aditya Shenoy <79261869+AdityaShen@users.noreply.github.com>

--- Labs/API_Labs/FastAPI_Labs/src/predict.py
+++ Labs/API_Labs/FastAPI_Labs/src/predict.py
@@ -1,13 +1,20 @@
 import joblib
+import numpy as np
 
-def predict_data(X):
+# Load model and scaler once at module level
+model = joblib.load("../model/digits_knn.pkl")
+scaler = joblib.load("../model/digits_scaler.pkl")
+
+
+def predict_digit(data):
     """
-    Predict the class labels for the input data.
+    Predict the digit class (0-9) for a single sample.
     Args:
-        X (numpy.ndarray): Input data for which predictions are to be made.
+        data: DigitData object with a 'pixels' list of 64 floats.
     Returns:
-        y_pred (numpy.ndarray): Predicted class labels.
+        int: Predicted digit (0-9).
     """
-    model = joblib.load("../model/iris_model.pkl")
-    y_pred = model.predict(X)
-    return y_pred
+    features = np.array(data.pixels).reshape(1, -1)
+    features_scaled = scaler.transform(features)
+    prediction = model.predict(features_scaled)
+    return int(prediction[0])
